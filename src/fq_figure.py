3f887ba40b2c54a36d97ec75461ed246ecae07d1
qiyanwei1 <qiyanweii@icloud.com>
2024-02-25 19:34:00 +0800

    staring programming

---
+++ src/fq_figure.py
@@ -0,0 +1,35 @@
+import re, os, sys, time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams['figure.dpi'] = 300
+palette = dict(A="tab:red", T="tab:green", C="tab:blue", G="tab:purple", S='tab:black')
+
+def plot_length_Nx_average_bar(read_length_list):
+
+def plot_read_length_frequency_distribution(read_length_list):
+def plot_read_length_cumulative_distribution(read_length_list):
+
+def plot_gc_content_frequency_distribution(gc_content_list):
+
+def plot_read_quality_frequency_distritution(read_quality_list):
+
+def plot_length_quality_cross_scatter(read_qual_list, read_length_list):
+def plot_kmer_spectrum_heatmap(kmer_list, kmer_count):
+
+def plot_homopolymer_frequency(homopolymer_dict):
+
+def plot_ends_base_content_curve():
+
+def plot_ends_base_quality_curve():
+
+def plot_read_percent_qualtiy_curve():
+
+
+
+
+
+
+

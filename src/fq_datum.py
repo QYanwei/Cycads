e431e4b6efa7074f7ae348df0deecb3be354e58c
qiyanwei1 <qiyanweii@icloud.com>
2024-02-25 19:33:24 +0800

    finished first version tested

--- src/fq_datum.py
+++ src/fq_datum.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 import re,os,sys,time
+import json
 import pyfastx
 
 import numpy as np
@@ -36,6 +37,8 @@ def endBaseTailParse(seq, shift_length, endBaseQual_dict):
     return endBaseQual_dict
 def endQualHeadParse(seq, quali, shift_length, endBaseQual_dict):
     i = 0
+    if '@' in seq:
+        print(seq)
     for b in seq[:shift_length]:
         endBaseQual_dict['HeadQualContent_dict'][b][i] += quali[:shift_length][i]
         i += 1
@@ -105,7 +108,7 @@ def homopolymerParse(seq, homopolymer_size_min, homopolymer_dict):
     return homopolymer_dict
 
 def readParse(read, seqdict):
-    seqdict['ID'].append(read.name)
+    seqdict['ID'].append(read.id)
     seqdict['GC'].append(readGCcontent(read.seq))
     seqdict['LEN'].append(len(read.seq))
     seqdict['QUAL1'].append(float(read.name.split('_')[-1]))
@@ -150,23 +153,58 @@ def overall_analyser(fq):
                     'HeadQualContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length, 'S':0},
                     'TailQualContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length, 'S':0},
                     }
+    split_part_num = 100
+    allBaseQual_dict = {
+                    'PercentBaseQual_dict': {'Q':[0]* split_part_num, 'S':0} # Q: average quality, S: base count
+    }
     for read in fq:
         seqdict = readParse(read, seqdict)
         homopolymer_dict = homopolymerParse(read.seq, homopolymer_size_min, homopolymer_dict)
         endBaseQual_dict = endBaseQualParse(read.seq, read.quali, shift_length, endBaseQual_dict)
-    return seqdict, homopolymer_dict, endBaseQual_dict
+        allBaseQual_dict = allBaseQualParse(read.quali, split_part_num, allBaseQual_dict)
+    return seqdict, homopolymer_dict, endBaseQual_dict, allBaseQual_dict
 
-fq = pyfastx.Fastq('../test/ecoli.fq.gz')
-seqdict1, homopolymer_dict1, endBaseQual_dict1, allBaseQual_dict1  = sampling_analyser(fq, 1, 100)
-seqdict2, homopolymer_dict2, endBaseQual_dict2 = overall_analyser(fq)
+# fq = pyfastx.Fastq('../test/ecoli.fq.gz')
+# seq_qual_dict1, homopolymer_dict1, endBaseQual_dict1, allBaseQual_dict1  = sampling_analyser(fq, 1, 100)
+# seq_qual_dict2, homopolymer_dict2, endBaseQual_dict2, allBaseQual_dict2 = overall_analyser(fq)
 
-for k, v in endBaseQual_dict1.items():
-    for i, j in v.items():
-        print(k, i, j)
-
-for k, v in endBaseQual_dict2.items():
-    for i, j in v.items():
-        print(k, i, j)
-
-for k, v in allBaseQual_dict1.items():
-    print(k,v)
+def get_fq_datum(fastq, mode):
+    if mode == 'sampling':
+        fq = pyfastx.Fastq(fastq)
+        seq_qual_dict, homopolymer_dict, endBaseQual_dict, allBaseQual_dict = sampling_analyser(fq, 1, 100)
+        sampling_fq_datum_dict = {
+            'seq_qual_dict': seq_qual_dict,
+            'homopolymer_dict': homopolymer_dict,
+            'endBaseQual_dict': endBaseQual_dict,
+            'allBaseQual_dict': allBaseQual_dict
+        }
+        return sampling_fq_datum_dict
+    elif mode == 'overall':
+        fq = pyfastx.Fastq(fastq)
+        seq_qual_dict, homopolymer_dict, endBaseQual_dict, allBaseQual_dict = overall_analyser(fq)
+        overall_fq_datum_dict = {
+            'seq_qual_dict': seq_qual_dict,
+            'homopolymer_dict': homopolymer_dict,
+            'endBaseQual_dict': endBaseQual_dict,
+            'allBaseQual_dict': allBaseQual_dict
+        }
+        return overall_fq_datum_dict
+    else:
+        print('please check the mode of parsing target data!')
+fastq = '../test/ecoli.fq.gz'
+# mode = 'sampling'
+mode = 'overall'
+merged_fq_datum_dict = get_fq_datum(fastq, mode)
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+with open( '../test/ecoli.seq.json', 'w') as file:
+    json.dump(merged_fq_datum_dict, file, indent=4,
+              separators=(', ', ': '), ensure_ascii=False,
+              cls=NpEncoder)

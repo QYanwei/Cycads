853edcd1fb17f2827152af7bfb829b3f9c526df6
qiyanwei1 <qiyanweii@icloud.com>
2024-02-26 21:12:46 +0800

    released

---
+++ src/fq_filter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import re,os,sys,time
+import pyfastx
+
+import numpy as np
+from threading import Thread
+from collections import Counter
+
+def fq_extract(fq):
+    a = 0
+def fq_filtering(fq):
+    a = 0
+def fq_cutting(fq):
+    a = 0
+def fq_downsampling(fq):
+    a = 0
--- src/fq_table.py
+++ src/fq_table.py
@@ -1,5 +1,12 @@
-import pyfastx
+#!/usr/bin/env python3
+# coding: utf-8
+import os
 
-fq = pyfastx.Fastq('../test/ecoli.fq.gz')
+input_fastq = '../test/ecoli.fq.gz'
 
+pyfastx_tool = "../tool/pyfastx"
 
+def pyfastx_stat_fq_and_idx(pyfastx_tool, input_fastq):
+    os.system(pyfastx_tool + ' stat ' +  input_fastq  + ' > '+ '../test.pyfqx.txt')
+
+pyfastx_stat_fq_and_idx(pyfastx_tool, input_fastq)

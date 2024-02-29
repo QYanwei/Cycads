b334e89fe12c05f2f14bbdd1792ac3c126772586
qiyanwei1 <qiyanweii@icloud.com>
2024-02-29 10:02:08 +0800

    updated

--- src/fq_table.py
+++ src/fq_table.py
@@ -7,6 +7,6 @@ input_fastq = '../test/ecoli.fq.gz'
 pyfastx_tool = "../tool/pyfastx"
 
 def pyfastx_stat_fq_and_idx(pyfastx_tool, input_fastq):
-    os.system(pyfastx_tool + ' stat ' +  input_fastq  + ' > '+ '../test.pyfqx.txt')
+    os.system(pyfastx_tool + ' stat ' +  input_fastq  + ' > '+ '../test/test.pyfqx.txt')
 
 pyfastx_stat_fq_and_idx(pyfastx_tool, input_fastq)

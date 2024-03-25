fe73ed628ad35ec9ef94b71e1070508f88c4738f
qiyanwei1 <qiyanweii@icloud.com>
2024-03-25 20:20:45 +0800

    restored

---
+++ cycad/bam_figure.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import re, os, sys, time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+import json
+import warnings
+warnings.filterwarnings("ignore", "is_categorical_dtype")
+warnings.filterwarnings("ignore", "use_inf_as_na")
+
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams['figure.dpi'] = 300
+palette = dict(A="tab:red", T="tab:green", C="tab:blue", G="tab:purple", S='tab:black')
+
+def plot_query_event_rate_overlapping_densities(**bam_datum_dict):
+    # identity rate
+    qry_idy_rate = np.array(bam_datum_dict['qry_idy_rate'])
+    qry_hpm_idy_rate = np.array(bam_datum_dict['qry_hpm_idy_rate'])
+    qry_non_hpm_idy_rate = np.array(bam_datum_dict['qry_non_hpm_idy_rate'])
+    qry_idy_df = pd.DataFrame([qry_idy_rate,qry_hpm_idy_rate, qry_non_hpm_idy_rate])
+    qry_idy_df.index = ['overall identity rate', 'homopolymer identity rate', 'non-homopolymer identity rate']
+    plt.clf()
+    fig, ax = plt.subplots()
+    sns.displot(data=qry_idy_df.T, kind="kde")
+    plt.yticks(fontproperties='Times New Roman', size=ticksize)
+    plt.xticks(fontproperties='Times New Roman', size=ticksize)
+    # ax.set_xlabel('x', fontsize=fontsize)
+    # ax.set_ylabel('y', fontsize=fontsize)
+    # ax.set_title('Title', fontsize=fontsize)
+    plt.tight_layout()
+    plt.savefig('../test/query_events_curve_idy' + '.dispplot.png')
+    # error rate
+    qry_dif_rate = np.array(bam_datum_dict['qry_dif_rate'])
+    qry_hpm_dif_rate = np.array(bam_datum_dict['qry_hpm_dif_rate'])
+    qry_non_hpm_dif_rate = np.array(bam_datum_dict['qry_non_hpm_dif_rate'])
+    qry_dif_df = pd.DataFrame([qry_dif_rate,qry_hpm_dif_rate, qry_non_hpm_dif_rate])
+    qry_dif_df.index = ['overall error rate', 'homopolymer error rate', 'non-homopolymer error rate']
+    plt.clf()
+    fig, ax = plt.subplots()
+    sns.displot(data=qry_dif_df.T, kind="kde")
+    plt.yticks(fontproperties='Times New Roman', size=ticksize)
+    plt.xticks(fontproperties='Times New Roman', size=ticksize)
+    # ax.set_xlabel('x', fontsize=fontsize)
+    # ax.set_ylabel('y', fontsize=fontsize)
+    # ax.set_title('Title', fontsize=fontsize)
+    plt.tight_layout()
+    plt.savefig('../test/query_events_curve_dif.dispplot.png')
+
+def plot_substitution_frequency(**overall_aln_event_stat_dict):
+    substitution = np.array(list(overall_aln_event_stat_dict['all_mis_typ_dict'].values()))
+    all_mis_typ_dict = dict(zip(overall_aln_event_stat_dict['all_mis_typ_dict'], substitution * 100 / sum(substitution) ))
+    substitution_type = pd.DataFrame(all_mis_typ_dict, index=[0])
+    substitution_type.index = ['Substitution']
+    plt.clf()
+    fig, ax = plt.subplots()
+    sns.barplot(data=substitution_type)
+    plt.yticks(fontproperties='Times New Roman', size=ticksize)
+    plt.xticks(fontproperties='Times New Roman', size=ticksize)
+    # ax.set_xlabel('x', fontsize=fontsize)
+    # ax.set_ylabel('y', fontsize=fontsize)
+    # ax.set_title('Title', fontsize=fontsize)
+    plt.tight_layout()
+    plt.savefig('../test/query_all_substitution_errors.barplot.png')
+
+def plot_insertion_deletion_frequency(**bam_datum_dict):
+    indel_len_max = 10
+    indel_range_dict = {'Insertion': {}, 'Deletion':{} }
+    ins_sum = sum(bam_datum_dict['all_ins_len_dict'].values())
+    del_sum = sum(bam_datum_dict['all_del_len_dict'].values())
+    for i in bam_datum_dict['all_ins_len_dict'].keys():
+        if int(i) < indel_len_max:
+            indel_range_dict['Insertion'][int(i)] = bam_datum_dict['all_ins_len_dict'][i] * 100 / ins_sum
+        elif int(i) >= indel_len_max:
+            if indel_len_max in indel_range_dict['Insertion'].keys():
+                indel_range_dict['Insertion'][indel_len_max] += bam_datum_dict['all_ins_len_dict'][i]
+            else:
+                indel_range_dict['Insertion'][indel_len_max] = bam_datum_dict['all_ins_len_dict'][i]
+        else:
+            pass
+    for i in bam_datum_dict['all_del_len_dict'].keys():
+        if int(i) < indel_len_max:
+            indel_range_dict['Deletion'][int(i)] = bam_datum_dict['all_del_len_dict'][i] * 100 / del_sum
+        elif int(i) >= indel_len_max:
+            if indel_len_max in indel_range_dict['Deletion'].keys():
+                indel_range_dict['Deletion'][indel_len_max] += bam_datum_dict['all_del_len_dict'][i]
+            else:
+                indel_range_dict['Deletion'][indel_len_max] = bam_datum_dict['all_del_len_dict'][i]
+        else:
+            pass
+    indel_range_dict['Insertion'][indel_len_max] = indel_range_dict['Insertion'][indel_len_max] * 100 / ins_sum
+    indel_range_dict['Deletion'][indel_len_max] = indel_range_dict['Deletion'][indel_len_max] * 100 / del_sum
+    Ins_dataframe = pd.DataFrame(indel_range_dict['Insertion'], index=[0])
+    Ins_dataframe.index = ['Insertion']
+    # Insertion
+    plt.clf()
+    fig, ax = plt.subplots()
+    sns.barplot(data=Ins_dataframe)
+    plt.yticks(fontproperties='Times New Roman', size=ticksize)
+    plt.xticks(fontproperties='Times New Roman', size=ticksize)
+    # ax.set_xlabel('x', fontsize=fontsize)
+    # ax.set_ylabel('y', fontsize=fontsize)
+    # ax.set_title('Title', fontsize=fontsize)
+    plt.tight_layout()
+    plt.savefig('../test/query_insertion_frequency' + '.barplot.png')
+    # Deletion
+    Del_dataframe = pd.DataFrame(indel_range_dict['Deletion'], index=[0])
+    Del_dataframe.index = ['Deletion']
+    plt.clf()
+    fig, ax = plt.subplots()
+    sns.barplot(data=Del_dataframe)
+    plt.yticks(fontproperties='Times New Roman', size=ticksize)
+    plt.xticks(fontproperties='Times New Roman', size=ticksize)
+    # ax.set_xlabel('x', fontsize=fontsize)
+    # ax.set_ylabel('y', fontsize=fontsize)
+    # ax.set_title('Title', fontsize=fontsize)
+    plt.tight_layout()
+    plt.savefig('../test/query_deletion_frequency' + '.barplot.png')
+
+def plot_overall_homopolymer_length_event_frequency(homopolymer_aln_event_stat_dict):
+    qry_hpm_event = np.array([homopolymer_aln_event_stat_dict['S'][i] for i in homopolymer_aln_event_stat_dict['S'].keys()])
+    qry_hpm_event2= []
+    for i in qry_hpm_event:
+        lt = []
+        p = 0
+        if sum(i) > 0:
+            for j in i /sum(i):
+                p += j
+                lt.append(p)
+        else:
+            pass
+        qry_hpm_event2.append(lt)
+    homopolymer_dataframe = pd.DataFrame(qry_hpm_event2)
+    homopolymer_dataframe.index = range(2,len(qry_hpm_event2)+2,1)
+    homopolymer_dataframe.columns = ["contraction 4bp+", "contraction 3bp", "contraction 2bp", "contraction 1bp",
+                                     "correct segment",  "mismatch segment",
+                                     "expansion 1bp", "expansion 2bp", "expansion 3bp", "expansion 4bp+"]
+    plt.clf()
+    fig, ax = plt.subplots()
+    sns.lineplot(data=homopolymer_dataframe)
+    plt.yticks(fontproperties='Times New Roman', size=ticksize)
+    plt.xticks(fontproperties='Times New Roman', size=ticksize)
+    # ax.set_xlabel('x', fontsize=fontsize)
+    # ax.set_ylabel('y', fontsize=fontsize)
+    # ax.set_title('Title', fontsize=fontsize)
+    plt.tight_layout()
+    plt.savefig('../test/query_homopolymer_length_event' + '.lineplot.png')
+
+def plot_overall_alignment_frequency(**overall_aln_event_sum_dict):
+    all_event = overall_aln_event_sum_dict['identity'] + overall_aln_event_sum_dict['substitution'] + overall_aln_event_sum_dict['contraction'] + overall_aln_event_sum_dict['expansion']
+    all_dif_event = overall_aln_event_sum_dict['substitution'] + overall_aln_event_sum_dict['contraction'] + overall_aln_event_sum_dict['expansion']
+    all_dif = all_dif_event / all_event *100
+    all_mis = overall_aln_event_sum_dict['substitution'] / all_event *100
+    all_del = overall_aln_event_sum_dict['contraction'] / all_event *100
+    all_ins = overall_aln_event_sum_dict['expansion'] / all_event *100
+    non_hpm_dif_event = overall_aln_event_sum_dict['non_hpm_substitution'] + overall_aln_event_sum_dict['non_hpm_contraction'] + overall_aln_event_sum_dict['non_hpm_expansion']
+    non_hpm_dif = non_hpm_dif_event / all_event *100
+    non_hpm_mis = overall_aln_event_sum_dict['non_hpm_substitution'] / all_event *100
+    non_hpm_del = overall_aln_event_sum_dict['non_hpm_contraction'] / all_event *100
+    non_hpm_ins = overall_aln_event_sum_dict['non_hpm_expansion'] / all_event *100
+    plt.clf()
+    all_aln_rates = [all_dif, all_mis, all_del, all_ins]
+    non_hpm_aln_rates = [non_hpm_dif, non_hpm_mis, non_hpm_del, non_hpm_ins]
+    labels = ['Error rate', 'Mismatch', 'Deletion', 'Insertion']
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+    fig, ax = plt.subplots()
+    ax.bar(x - width/2, all_aln_rates, width, label='Total error rate')
+    ax.bar(x + width/2, non_hpm_aln_rates, width, label='Non-homopolymer error rate')
+    ax.set_ylabel('Percentage(%)')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    plt.yticks(fontproperties='Times New Roman', size=ticksize)
+    plt.xticks(fontproperties='Times New Roman', size=ticksize)
+    # ax.set_xlabel('x', fontsize=fontsize)
+    # ax.set_ylabel('y', fontsize=fontsize)
+    # ax.set_title('Title', fontsize=fontsize)
+    ax.legend()
+    plt.tight_layout()
+    plt.savefig('../test/query_all_error_item' + '.barplot.png')
+
+fontsize=16
+ticksize=18
+
+bam_json_file_path = '../test/ecoli.bam.json'
+with open(bam_json_file_path) as jsonfile:
+    bam_datum_dict = json.loads(json.dumps(eval(jsonfile.read()))) # pprint: ' -> json: "
+    plot_query_event_rate_overlapping_densities(**bam_datum_dict['query_aln_event_stat_dict'])
+    plot_overall_homopolymer_length_event_frequency(bam_datum_dict['homopolymer_aln_event_stat_dict'])
+    plot_insertion_deletion_frequency(**bam_datum_dict['overall_aln_event_stat_dict'])
+    plot_overall_alignment_frequency(**bam_datum_dict['overall_aln_event_sum_dict'])
+    plot_substitution_frequency(**bam_datum_dict['overall_aln_event_stat_dict'])
---
+++ cycad/fq_datum.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import re
+import os
+import pprint
+import pyfastx
+
+import numpy as np
+from collections import Counter
+
+def readGCcontent(seq, seq_len):
+    gc_count = 0
+    for base in seq:
+        if base == 'G' or base == 'C':
+            gc_count += 1
+    return round(gc_count/seq_len, 3)
+
+def readAvgQscore(quali, seq_len):
+    value_list = Counter(list(quali))
+    value_sum = sum([k * v for k, v in value_list.items()])
+    seq_qscore = round(value_sum / seq_len, 3)
+    return seq_qscore
+
+def endBaseHeadParse(seq, shift_length, endBaseQual_dict):
+    i = 0
+    for b in seq[:shift_length]:
+        endBaseQual_dict['HeadBaseContent_dict'][b][i] += 1
+        i += 1
+    return endBaseQual_dict
+def endBaseTailParse(seq, shift_length, endBaseQual_dict):
+    i = 0
+    for b in seq[-shift_length:]:
+        endBaseQual_dict['TailBaseContent_dict'][b][i] += 1
+        i += 1
+    return endBaseQual_dict
+def endQualHeadParse(seq, quali, shift_length, endBaseQual_dict):
+    i = 0
+    for b in seq[:shift_length]:
+        endBaseQual_dict['HeadQualContent_dict'][b][i] += quali[:shift_length][i]
+        endBaseQual_dict['HeadQualContent_dict']['S'][b][i] += 1
+        i += 1
+    return endBaseQual_dict
+def endQualTailParse(seq, quali, shift_length, endBaseQual_dict):
+    i = 0
+    for b in seq[-shift_length:]:
+        endBaseQual_dict['TailQualContent_dict'][b][i] += quali[-shift_length:][i]
+        endBaseQual_dict['TailQualContent_dict']['S'][b][i] += 1
+        i += 1
+    return endBaseQual_dict
+def read_quality_to_bin_score(base_qual_list: list, split_number_of_read_length: int):
+    div, mod = divmod(len(base_qual_list), split_number_of_read_length)
+    qs_in_percentage_pos = [sum(base_qual_list[i * div + min(i, mod):(i + 1) * div + min(i + 1, mod)]) / len(
+        base_qual_list[i * div + min(i, mod):(i + 1) * div + min(i + 1, mod)]) for i in
+                            range(split_number_of_read_length)]
+    return qs_in_percentage_pos
+def allBaseQualParse(quali, split_part_num, allBaseQual_dict):
+    if len(quali) >= split_part_num:
+        qv_in_percentage_pos = read_quality_to_bin_score(quali, split_part_num)
+        for j in range(split_part_num):
+            allBaseQual_dict['PercentBaseQual_dict']['Q'][j] += qv_in_percentage_pos[j]
+        allBaseQual_dict['PercentBaseQual_dict']['S'] += 1
+    return allBaseQual_dict
+
+def endBaseQualParse(seq, quali, shift_length, endBaseQual_dict):
+    if len(seq) > shift_length * 2:
+        endBaseQual_dict = endBaseHeadParse(seq, shift_length, endBaseQual_dict)
+        endBaseQual_dict = endBaseTailParse(seq, shift_length, endBaseQual_dict)
+        endBaseQual_dict = endQualHeadParse(seq, quali, shift_length, endBaseQual_dict)
+        endBaseQual_dict = endQualTailParse(seq, quali, shift_length, endBaseQual_dict)
+        endBaseQual_dict['HeadBaseContent_dict']['S'] += 1
+        endBaseQual_dict['TailBaseContent_dict']['S'] += 1
+    return endBaseQual_dict
+def homopolymerParse(seq, homopolymer_size_min, homopolymer_dict):
+    ATCG = ['A', 'G', 'C', 'T']
+    for base in ATCG:
+        patterns = re.compile(r"%s{%d,}" % (base, homopolymer_size_min))
+        homostring_dict = dict(Counter(patterns.findall(seq)))
+        if len(homostring_dict):
+            for homostring, homofreq in homostring_dict.items():
+                homolen = len(homostring)
+                if homolen in  homopolymer_dict[base].keys():
+                    homopolymer_dict[base][homolen] += homofreq
+                else:
+                    homopolymer_dict[base][homolen] = homofreq
+    return homopolymer_dict
+def kmerSpectrumParse(fq_path, kmer_size, output_dir):
+    kmersize = kmer_size
+    output = output_dir
+    pwd_config_file = os.path.realpath(__file__)
+    meryl = '/'.join(pwd_config_file.split('/')[:-1]) + '/tools/meryl'
+    if fq_path.endswith('gz'):
+        os.system('gunzip -c {}|awk \'NR %4 == 1 || NR %4 == 2 \'   > {}.fasta '.format(fq_path, output))
+        os.system('{} count  k={} {}.fasta output {}.meryl'.format(meryl, str(kmersize), output, output))
+        os.system('{} print {}.meryl |sort -k2nr > {}_kmer_{}_freq.txt'.format(meryl, output, output, str(kmersize)))
+        os.system('rm -f {}.fasta'.format(output))
+        os.system('rm -rf {}.meryl'.format(output))
+    elif fq_path.endswith('fastq') or fq_path.endswith('fq'):
+        os.system('awk \'NR %4 == 1 || NR %4 == 2 \'   > {}.fasta '.format(fq_path, output))
+        os.system('{} count  k={} {}.fasta output {}.meryl'.format(meryl, str(kmersize), output, output))
+        os.system('{} print {}.meryl | sort -k2nr > {}_kmer_{}_freq.txt'.format(meryl, output, output, str(kmersize)))
+        os.system('rm -f {}.fasta'.format(output))
+        os.system('rm -rf {}.meryl'.format(output))
+    else:
+        print('please determining the input file suffix is fastq or fq or fq.gz!')
+# kmerSpectrumParse('../test/ecoli.fq.gz', 5, '../test/')
+
+def readParse(number, name, seq, quali, seqdict):
+    seq_len = len(seq)
+    seqdict['ID'].append(number)
+    seqdict['GC'].append(readGCcontent(seq, seq_len))
+    seqdict['LEN'].append(seq_len)
+    seqdict['QUAL1'].append(float(name.split('_')[-1]))
+    seqdict['QUAL2'].append(readAvgQscore(quali, seq_len))
+    return seqdict
+def random_readnum(seed_num, read_size, sample_num):
+    np.random.seed(seed_num)
+    sample_list = np.random.randint(1, read_size, (1, sample_num) )
+    return sample_list
+def sampling_analyser(fq, seed_num, read_num):
+    seqdict = dict( {'ID': [], 'GC': [], 'LEN': [], 'QUAL1': [], 'QUAL2':[]} ) # QUAL1: read basecall Q, QUAL2: read average Q
+    homopolymer_size_min = 5
+    homopolymer_dict = {'A':{}, 'G':{}, 'C':{}, 'T':{}}
+    shift_length = 200
+    endBaseQual_dict = {
+                    'HeadBaseContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length, 'S':0},
+                    'TailBaseContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length, 'S':0},
+                    'HeadQualContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length,
+                                             'S':{'A':[0]*shift_length, 'T':[0]*shift_length, 'G':[0]*shift_length, 'C':[0]*shift_length}},
+                    'TailQualContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length,
+                                             'S':{'A':[0]*shift_length, 'T':[0]*shift_length, 'G':[0]*shift_length, 'C':[0]*shift_length}},
+                    }
+    split_part_num = 100
+    allBaseQual_dict = {
+                    'PercentBaseQual_dict': {'Q':[0]* split_part_num, 'S':0}} # Q: average quality, S: base count
+    sample_list = random_readnum(seed_num, len(fq), read_num)[0]
+    fq = pyfastx.Fastq(fastq)
+    for i in sample_list:
+        read = fq[i]
+        seqdict = readParse(i, read.name, read.seq, read.quali, seqdict)
+        homopolymer_dict = homopolymerParse(read.seq, homopolymer_size_min, homopolymer_dict)
+        endBaseQual_dict = endBaseQualParse(read.seq, read.quali, shift_length, endBaseQual_dict)
+        allBaseQual_dict = allBaseQualParse(read.quali, split_part_num, allBaseQual_dict)
+    return seqdict, homopolymer_dict, endBaseQual_dict, allBaseQual_dict
+
+def overall_analyser(fq):
+    seqdict = dict({'ID': [], 'GC': [], 'LEN': [], 'QUAL1': [], 'QUAL2': []})  # QUAL1: read basecall Q, QUAL2: read average Q
+    homopolymer_size_min = 5
+    homopolymer_dict = {'A':{}, 'G':{}, 'C':{}, 'T':{}}
+    shift_length = 200
+    endBaseQual_dict = {
+                    'HeadBaseContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length, 'S':0},
+                    'TailBaseContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length, 'S':0},
+                    'HeadQualContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length,
+                                             'S':{'A':[0]*shift_length, 'T':[0]*shift_length, 'G':[0]*shift_length, 'C':[0]*shift_length}},
+                    'TailQualContent_dict': {'A': [0]*shift_length, 'G': [0]*shift_length, 'C': [0]*shift_length, 'T': [0]*shift_length,
+                                             'S':{'A':[0]*shift_length, 'T':[0]*shift_length, 'G':[0]*shift_length, 'C':[0]*shift_length}},
+                    }
+    split_part_num = 100
+    allBaseQual_dict = {
+                    'PercentBaseQual_dict': {'Q':[0]* split_part_num, 'S':0}} # Q: average quality, S: base count
+    number = 0
+    fq = pyfastx.Fastq(fq, build_index=False)
+    for name, seq, qual in fq:
+        number += 1
+        quali = [ord(i) - 33 for i in qual]
+        seqdict = readParse(number, name, seq, quali, seqdict)
+        homopolymer_dict = homopolymerParse(seq, homopolymer_size_min, homopolymer_dict)
+        endBaseQual_dict = endBaseQualParse(seq, quali, shift_length, endBaseQual_dict)
+        allBaseQual_dict = allBaseQualParse(quali, split_part_num, allBaseQual_dict)
+    return seqdict, homopolymer_dict, endBaseQual_dict, allBaseQual_dict
+
+# fq = pyfastx.Fastq('../test/ecoli.fq.gz')
+# seq_qual_dict1, homopolymer_dict1, endBaseQual_dict1, allBaseQual_dict1  = sampling_analyser(fq, 1, 100)
+# seq_qual_dict2, homopolymer_dict2, endBaseQual_dict2, allBaseQual_dict2 = overall_analyser(fq)
+
+def get_fq_datum(fastq, mode):
+    if mode == 'sampling':
+        seq_qual_dict, homopolymer_dict, endBaseQual_dict, allBaseQual_dict = sampling_analyser(fastq, 1, 100)
+        sampling_fq_datum_dict = {
+            'seq_qual_dict': seq_qual_dict,
+            'homopolymer_dict': homopolymer_dict,
+            'endBaseQual_dict': endBaseQual_dict,
+            'allBaseQual_dict': allBaseQual_dict
+        }
+        return sampling_fq_datum_dict
+    elif mode == 'overall':
+        seq_qual_dict, homopolymer_dict, endBaseQual_dict, allBaseQual_dict = overall_analyser(fastq)
+        overall_fq_datum_dict = {
+            'seq_qual_dict': seq_qual_dict,
+            'homopolymer_dict': homopolymer_dict,
+            'endBaseQual_dict': endBaseQual_dict,
+            'allBaseQual_dict': allBaseQual_dict
+        }
+        return overall_fq_datum_dict
+    else:
+        print('please check the mode of parsing target data!')
+
+if __name__ == "__main__" :
+    # parameter list
+    fastq = '../test/ecoli.fq.gz'
+    # mode = 'sampling'
+    mode = 'overall'
+    # initiated dict
+
+    # analyzing data
+    merged_fq_datum_dict = get_fq_datum(fastq, mode)
+
+    # structure write
+    with open( '../test/ecoli.seq.json', 'w') as jsonfile:
+        filewidth = len(merged_fq_datum_dict['seq_qual_dict']['ID']) + 60
+        pprint.pprint(merged_fq_datum_dict, jsonfile, indent=4, width=filewidth, depth = 6, compact=True)

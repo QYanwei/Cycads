66fef15b95a5b362fa10273ea6f731537866ef0b
Jia-Yuan Zhang <sloppybaron@gmail.com>
2024-03-29 02:52:37 +0000

    Update FASTQ figures

--- cycad/fq_figure.py
+++ cycad/fq_figure.py
@@ -20,6 +20,7 @@ palette = dict(A="tab:red", T="tab:green", C="tab:blue", G="tab:purple", S='tab:
 figsize = (5, 4)
 hist_kw = dict(facecolor='tab:blue', edgecolor='k', linewidth=0.5)
 grid_kw = dict(color='k', alpha=0.1)
+title_kw = dict(fontsize=10)
 
 
 def post_process_ax(ax):
@@ -52,6 +53,7 @@ def plot_length_Nx_average_bar(seq_qual_dict):
     ax.set_xlabel("")
     ax.set_ylabel("Read length (bp)")
     ax.grid(axis='y', **grid_kw)
+    ax.set_title("Read length Nx metrics", **title_kw)
     post_process_ax(ax)
     return fig
 
@@ -65,6 +67,7 @@ def plot_gc_content_frequency_distribution(seq_qual_dict):
     ax.set_xticks(list(range(0, 101, 10)))
     ax.set_ylabel("Density")
     ax.set_yticks([])
+    ax.set_title("Read GC content distribution", **title_kw)
     post_process_ax(ax)
     return fig
 
@@ -79,6 +82,7 @@ def plot_read_quality_frequency_distritution(seq_qual_dict):
     ax.set_xlim(xmin, xmax)
     ax.set_ylabel("Density")
     ax.set_yticks([])
+    ax.set_title("Mean base quality distribution", **title_kw)
     post_process_ax(ax)
     return fig
 
@@ -91,6 +95,7 @@ def plot_read_length_frequency_distribution(seq_qual_dict):
     ax.set_xlabel("Read length (kb)")
     ax.set_ylabel("Density")
     ax.set_yticks([])
+    ax.set_title("Read length distribution", **title_kw)
     post_process_ax(ax)
     return fig
 
@@ -108,20 +113,26 @@ def plot_read_length_cumulative_distribution(seq_qual_dict):
     ax.set_ylabel("Cumulative fraction of bases")
     ax.yaxis.set_major_formatter(mtick.PercentFormatter())
     ax.grid(axis='both', **grid_kw)
+    ax.set_title("Cumulative fraction of bases vs. read length", **title_kw)
     post_process_ax(ax)
     return fig
 
-def plot_length_quality_cross_scatter(seq_qual_dict, output):
-    LenQualFrame = pd.DataFrame( np.array([ seq_qual_dict['LEN'], seq_qual_dict['QUAL1'] ] ).T, columns = [ 'read_length', 'read_averageQ' ])
-    plt.clf()
-    sns.set_style("ticks")
-    ax = sns.scatterplot(data=LenQualFrame, x="read_length", y="read_averageQ", linewidth=0, size=1, alpha=0.5, legend=None)
-    bottom, top = ax.get_ylim()
-    ax.set_ylim(bottom + 0.2, top - 0.2)
-    # sns.scatterplot(data=LenQualFrame, x="read_length", y="read_averageQ", linewidth=0, size=2, alpha=0.3, legend=None)
-    plt.savefig(output+'/report_html/read_length_quality_cross' + '.scatterplot.png')
+def plot_length_quality_2d_histogram(seq_qual_dict):
+    lengths = np.array(seq_qual_dict['LEN']) / 1000
+    quality_values = np.array(seq_qual_dict['QUAL1'])
 
-def plot_homopolymer_frequency(homopolymer_dict, output):
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.hist2d(lengths, quality_values, bins=(50, 50), cmap='Blues')
+    ax.set_xlim(lengths.min(), lengths.max())
+    ax.set_ylim(quality_values.min(), quality_values.max())
+    ax.set_xlabel("Read length (kb)")
+    ax.set_ylabel("Mean per-read base quality")
+    ax.set_title("Joint distribution of read length and mean base quality", **title_kw)
+    post_process_ax(ax)
+    return fig
+    
+
+def plot_homopolymer_frequency(homopolymer_dict):
     homopolymer_len_max = 10
     homopolymer_range_dict = {'A': {}, 'G': {}, 'C': {}, 'T': {} }
     for b in homopolymer_dict.keys():
@@ -135,118 +146,161 @@ def plot_homopolymer_frequency(homopolymer_dict, output):
                     homopolymer_range_dict[b][homopolymer_len_max] += homopolymer_dict[b][i]
             else:
                 pass
-    plt.clf()
-    A_dataframe = pd.DataFrame([homopolymer_range_dict['A']])
-    A_dataframe.index = ['A']
-    T_dataframe = pd.DataFrame([homopolymer_range_dict['T']])
-    T_dataframe.index = ['T']
-    G_dataframe = pd.DataFrame([homopolymer_range_dict['G']])
-    G_dataframe.index = ['G']
-    C_dataframe = pd.DataFrame([homopolymer_range_dict['C']])
-    C_dataframe.index = ['C']
-    homopolymerRangeFrame = pd.concat([A_dataframe, T_dataframe, G_dataframe, C_dataframe], axis=0)
-    sns.lineplot(data=homopolymerRangeFrame.T, dashes=False)
-    plt.savefig(output+'/report_html/read_homopolymer_frequency' + '.lineplot.png')
+    xs = list(range(2, homopolymer_len_max + 1))
+    fig, ax = plt.subplots(figsize=figsize)
 
-# def plot_kmer_spectrum_heatmap(kmer_list, kmer_count):
+    for base in ("A", "T", "C", "G"):
+        ys = [homopolymer_range_dict[base][x] for x in xs]
+        color = palette[base]
+        ax.plot(xs, ys, label=base)
+    ax.legend(loc='upper right')
+    ax.set_xlim(min(xs), max(xs))
+    ax.set_xlabel("Homopolymer length")
+    ax.set_yscale("log")
+    ax.set_ylabel("Total number of occurances")
+    ax.set_title("Homopolymer length distribution", **title_kw)
+    post_process_ax(ax)
 
-def plot_ends_base_content_curve(endBaseQual_dict, output):
-    # head base content
-    plt.clf()
-    dataframe = pd.DataFrame([endBaseQual_dict['HeadBaseContent_dict']])
-    A_ratio = np.array(dataframe['A'][0])/dataframe['S'][0]
-    T_ratio = np.array(dataframe['T'][0])/dataframe['S'][0]
-    G_ratio = np.array(dataframe['G'][0])/dataframe['S'][0]
-    C_ratio = np.array(dataframe['C'][0])/dataframe['S'][0]
-    GC_ratio = (np.array(dataframe['G'][0]) + np.array(dataframe['C'][0]))/dataframe['S'][0]
-    numpyarray = np.array([A_ratio, T_ratio, G_ratio, C_ratio, GC_ratio]).T
-    PercentQualityFrame = pd.DataFrame(data=numpyarray, columns=['A', 'T', 'C', 'G', 'GC'])
-    sns.lineplot(data=PercentQualityFrame, dashes=False)
-    plt.savefig(output+'/report_html/read_head_base_content' + '.lineplot.png')
-    # tail base content
-    plt.clf()
-    dataframe = pd.DataFrame([endBaseQual_dict['TailBaseContent_dict']])
-    A_ratio = np.array(dataframe['A'][0])/dataframe['S'][0]
-    T_ratio = np.array(dataframe['T'][0])/dataframe['S'][0]
-    G_ratio = np.array(dataframe['G'][0])/dataframe['S'][0]
-    C_ratio = np.array(dataframe['C'][0])/dataframe['S'][0]
-    GC_ratio = (np.array(dataframe['G'][0]) + np.array(dataframe['C'][0]))/dataframe['S'][0]
-    numpyarray = np.array([A_ratio, T_ratio, G_ratio, C_ratio, GC_ratio]).T
-    PercentQualityFrame = pd.DataFrame(data=numpyarray, columns=['A', 'T', 'C', 'G', 'GC'])
-    sns.lineplot(data=PercentQualityFrame, dashes=False).invert_xaxis()
-    plt.savefig(output+'/report_html/read_tail_base_content' + '.lineplot.png')
+    return fig
 
-def plot_ends_base_quality_curve(endBaseQual_dict, output):
-    # head base content
-    plt.clf()
-    dataframe = pd.DataFrame([endBaseQual_dict['HeadQualContent_dict']])
-    basetotal = dataframe['S'][0]
-    A_quality = np.array(dataframe['A'][0])/np.array(basetotal['A'])
-    T_quality = np.array(dataframe['T'][0])/np.array(basetotal['T'])
-    G_quality = np.array(dataframe['G'][0])/np.array(basetotal['G'])
-    C_quality = np.array(dataframe['C'][0])/np.array(basetotal['C'])
-    base_total = np.array(basetotal['A']) + np.array(basetotal['T']) + np.array(basetotal['G']) + np.array(basetotal['C'])
-    Mean_quality = np.array( np.array(dataframe['A'][0]) + np.array(dataframe['T'][0]) + np.array(dataframe['G'][0]) + np.array(dataframe['C'][0]))/np.array(base_total)
-    numpyarray = np.array([A_quality, T_quality, G_quality, C_quality, Mean_quality]).T
-    PercentQualityFrame = pd.DataFrame(data=numpyarray, columns=['A', 'T', 'C', 'G', 'Mean'])
-    sns.lineplot(data=PercentQualityFrame, dashes=False)
-    plt.savefig(output+'/report_html/read_head_base_quality' + '.lineplot.png')
-    # tail base content
-    plt.clf()
-    dataframe = pd.DataFrame([endBaseQual_dict['TailQualContent_dict']])
-    basetotal = dataframe['S'][0]
-    A_quality = np.array(dataframe['A'][0])/np.array(basetotal['A'])
-    T_quality = np.array(dataframe['T'][0])/np.array(basetotal['T'])
-    G_quality = np.array(dataframe['G'][0])/np.array(basetotal['G'])
-    C_quality = np.array(dataframe['C'][0])/np.array(basetotal['C'])
-    base_total = np.array(basetotal['A']) + np.array(basetotal['T']) + np.array(basetotal['G']) + np.array(basetotal['C'])
-    Mean_quality = np.array( np.array(dataframe['A'][0]) + np.array(dataframe['T'][0]) + np.array(dataframe['G'][0]) + np.array(dataframe['C'][0]))/np.array(base_total)
-    numpyarray = np.array([A_quality, T_quality, G_quality, C_quality, Mean_quality]).T
-    PercentQualityFrame = pd.DataFrame(data=numpyarray, columns=['A', 'T', 'C', 'G', 'Mean'])
-    sns.lineplot(data=PercentQualityFrame, dashes=False)
-    plt.savefig(output+'/report_html/read_tail_base_quality' + '.lineplot.png')
 
-def plot_read_percent_qualtiy_curve(allBaseQual_dict, output):
-    plt.clf()
-    percent_pos_avg_quality = np.array(allBaseQual_dict['PercentBaseQual_dict']['Q'])/ allBaseQual_dict['PercentBaseQual_dict']['S']
-    numpyarray = np.array([percent_pos_avg_quality, np.arange(len(percent_pos_avg_quality))+1]).T
-    PercentQualityFrame = pd.DataFrame(data=numpyarray, columns=['average_quality', 'relative_position'])
-    sns.lineplot(data=PercentQualityFrame, x="relative_position", y="average_quality")
-    plt.savefig(output+'/report_html/read_relative_position_avg_qual' + '.lineplot.png')
+def plot_ends_base_content_curve(endBaseQual_dict):
+    for key in ("HeadBaseContent_dict", "TailBaseContent_dict"):
+        fig, ax = plt.subplots(figsize=figsize)
+        if key == "HeadBaseContent_dict":
+            fig1 = fig
+        else:
+            fig2 = fig
+        data = endBaseQual_dict[key]
+        for base in ("A", "T", "C", "G"):
+            ys = np.array(data[base]) / data['S']
+            ys *= 100
+            xs = np.arange(ys.shape[0])
+            if key == "TailBaseContent_dict":
+                xs = xs.max() - xs
+            color = palette[base]
+            ax.plot(xs, ys, color=color, ls='-', label=base)
+        ys = (np.array(data["G"]) + np.array(data["C"])) / data['S']
+        ys *= 100
+        ax.plot(xs, ys, color='tab:brown', ls='-', label="G+C")
+
+        ax.grid(axis='both', **grid_kw)
+        
+        if key == "HeadBaseContent_dict":
+            ax.set_xlim(0, xs.max() + 1)
+            ax.set_xlabel("Distance from 5' end")
+            ax.legend(loc='upper right')
+            ax.set_title("Base content near 5' end", **title_kw)
+        else:
+            ax.set_xlim(xs.max() + 1, 0)
+            ax.set_xlabel("Distance from 3' end")
+            ax.legend(loc='upper left')
+            ax.set_title("Base content near 3' end", **title_kw)
+            
+        ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+        ax.set_ylabel("Frequency of base")
+        post_process_ax(ax)
+    
+    return fig1, fig2
+
+def plot_ends_base_quality_curve(endBaseQual_dict):
+
+    for key in ("HeadQualContent_dict", "TailQualContent_dict"):
+        fig, ax = plt.subplots(figsize=figsize)
+        if key == "HeadQualContent_dict":
+            fig1 = fig
+        else:
+            fig2 = fig
+        data = endBaseQual_dict[key]
+        for base in ("A", "T", "C", "G"):
+            ys = np.array(data[base]) / np.array(data['S'][base])
+            xs = np.arange(ys.shape[0])
+            if key == "TailQualContent_dict":
+                xs = xs.max() - xs
+            color = palette[base]
+            ax.plot(xs, ys, color=color, ls='-', label=base)
+        ys = sum(np.array(quality_values) for key, quality_values in data.items() if key in ("A", "T", "C", "G")) / sum(np.array(base_counts) for key, base_counts in data['S'].items() if key in ("A", "T", "C", "G"))
+        ax.plot(xs, ys, color='tab:brown', ls='-', label="Overall")
+
+        ax.grid(axis='both', **grid_kw)
+        
+        if key == "HeadQualContent_dict":
+            ax.set_xlim(0, xs.max() + 1)
+            ax.set_xlabel("Distance from 5' end")
+            ax.legend(loc='lower right')
+            ax.set_title("Base content near 5' end", **title_kw)
+        else:
+            ax.set_xlim(xs.max() + 1, 0)
+            ax.set_xlabel("Distance from 3' end")
+            ax.legend(loc='lower left')
+            ax.set_title("Base content near 3' end", **title_kw)
+            
+        ax.set_ylabel("Mean base quality")
+        post_process_ax(ax)
+
+    return fig1, fig2
+    
+
+def plot_read_percent_qualtiy_curve(allBaseQual_dict):
+    quality_values = np.array(allBaseQual_dict['PercentBaseQual_dict']['Q'])/ allBaseQual_dict['PercentBaseQual_dict']['S']
+    xs = np.arange(1, quality_values.shape[0]+1)
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(xs, quality_values, clip_on=False, color='steelblue')
+    ax.set_xlim(0, 100)
+    ax.set_xlabel("Relative position in read")
+    ax.set_ylabel("Mean base quality")
+    ax.set_title("Mean base quality vs. relative position in read", **title_kw)
+    ax.grid(axis='both', **grid_kw)
+    post_process_ax(ax)
+    return fig
+
+    
 
 def fq_figure_action(args):
     output = args["sample_name"]
-    if os.path.exists(output + "/" + output +"_fq.json"):
-        fastq_json_file_path = args["sample_name"]+"/" + args["sample_name"]+"_fq.json"
-        with open(fastq_json_file_path) as jsonfile:
-            fq_datum_dict = json.loads(json.dumps(eval(jsonfile.read()))) # pprint: ' -> json: "
-            seq_qual_dict = fq_datum_dict['seq_qual_dict']
-            homopolymer_dict = fq_datum_dict['homopolymer_dict']
-            endBaseQual_dict = fq_datum_dict['endBaseQual_dict']
-            allBaseQual_dict = fq_datum_dict['allBaseQual_dict']
-
-        fig = plot_length_Nx_average_bar(seq_qual_dict)
-        fig.savefig(output+'/report_html/read_length_biostat' + '.barplot.png')
-
-        fig = plot_gc_content_frequency_distribution(seq_qual_dict)
-        fig.savefig(output+'/report_html/read_gc_histplot' + '.barplot.png')
-
-        fig = plot_read_quality_frequency_distritution(seq_qual_dict)
-        fig.savefig(output+'/report_html/read_quality_histplot' + '.barplot.png')
-
-        fig = plot_read_length_frequency_distribution(seq_qual_dict)
-        fig.savefig(output+'/report_html/read_length_histplot_nolog' + '.barplot.png')
-
-        fig = plot_read_length_cumulative_distribution(seq_qual_dict)
-        fig.savefig(output+'/report_html/read_length_cumulative' + '.barplot.png')
-
-        plot_length_quality_cross_scatter(seq_qual_dict, output)
-        plot_read_percent_qualtiy_curve(allBaseQual_dict, output)
-        plot_ends_base_content_curve(endBaseQual_dict, output)
-        plot_ends_base_quality_curve(endBaseQual_dict, output)
-        plot_homopolymer_frequency(homopolymer_dict, output)
-    else:
+    if not os.path.exists(output + "/" + output +"_fq.json"):
         raise IOError("FASTQ stat JSON file not found!")
+        
+    fastq_json_file_path = args["sample_name"]+"/" + args["sample_name"]+"_fq.json"
+    with open(fastq_json_file_path) as jsonfile:
+        fq_datum_dict = json.loads(json.dumps(eval(jsonfile.read()))) # pprint: ' -> json: "
+        seq_qual_dict = fq_datum_dict['seq_qual_dict']
+        homopolymer_dict = fq_datum_dict['homopolymer_dict']
+        endBaseQual_dict = fq_datum_dict['endBaseQual_dict']
+        allBaseQual_dict = fq_datum_dict['allBaseQual_dict']
+
+    fig = plot_length_Nx_average_bar(seq_qual_dict)
+    fig.savefig(output+'/report_html/read_length_biostat' + '.barplot.png')
+
+    fig = plot_gc_content_frequency_distribution(seq_qual_dict)
+    fig.savefig(output+'/report_html/read_gc_histplot' + '.barplot.png')
+
+    fig = plot_read_quality_frequency_distritution(seq_qual_dict)
+    fig.savefig(output+'/report_html/read_quality_histplot' + '.barplot.png')
+
+    fig = plot_read_length_frequency_distribution(seq_qual_dict)
+    fig.savefig(output+'/report_html/read_length_histplot_nolog' + '.barplot.png')
+
+    fig = plot_read_length_cumulative_distribution(seq_qual_dict)
+    fig.savefig(output+'/report_html/read_length_cumulative' + '.barplot.png')
+
+    fig = plot_length_quality_2d_histogram(seq_qual_dict)
+    fig.savefig(output+'/report_html/read_length_quality_cross' + '.scatterplot.png')
+
+    fig = plot_read_percent_qualtiy_curve(allBaseQual_dict)
+    fig.savefig(output+'/report_html/read_relative_position_avg_qual' + '.lineplot.png')
+
+    fig1, fig2 = plot_ends_base_content_curve(endBaseQual_dict)
+    fig1.savefig(output+'/report_html/read_head_base_content' + '.lineplot.png')
+    fig2.savefig(output+'/report_html/read_tail_base_content' + '.lineplot.png')
+
+    fig1, fig2 = plot_ends_base_quality_curve(endBaseQual_dict)
+    fig1.savefig(output+'/report_html/read_head_base_quality' + '.lineplot.png')
+    fig2.savefig(output+'/report_html/read_tail_base_quality' + '.lineplot.png')
+
+    fig = plot_homopolymer_frequency(homopolymer_dict)
+    fig.savefig(output+'/report_html/read_homopolymer_frequency' + '.lineplot.png')
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
